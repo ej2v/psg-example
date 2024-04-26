49b087c82877dc53b2fa79b04bed60790a17b115
Jonathan Villanueva <me@jevillanueva.dev>
2024-04-25 20:33:02 -0400

    Sesión 04

---
+++ sesion04.py
@@ -0,0 +1,12 @@
+# Example of execution flow
+# Instruction 1
+print("Start of the program")
+ 
+# Instruction 2
+print(" Print second instruction")
+ 
+# Instruction 3
+print("Hello, World!")
+ 
+# Instruction 4
+print("End of the program")
